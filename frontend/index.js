78158450742b3d02552da36328534e656397f644
Zairaxx <brandontsegai@gmail.com>
2023-04-05 09:56:42 +0200

    slutgiltig lösning

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,6 @@
 let todoList = document.querySelector("ul");
+let completedList = document.querySelector("#completed-list");
+
 let todoTitle = document.querySelector("#todoTitle");
 let todoDesc = document.querySelector("#todoDesc");
 //REGISTRATION
@@ -22,12 +24,88 @@ let renderPage = async () => {
     console.log(response.data.data);
     if (response.data) {
       todoList.innerHTML = "";
-      let todos = response.data.data;
+      completedList.innerHTML = "";
+      let todos = response.data.data.filter(
+        (todo) => !todo.attributes.completed
+      );
+      let completedTodos = response.data.data.filter(
+        (todo) => todo.attributes.completed
+      );
+
       todos.forEach((todo) => {
-        todoList.innerHTML += `<li> <b>Title</b>:${todo.attributes.title} <b>Description:</b> ${todo.attributes.description}</li>`;
+        todoList.innerHTML += `<li>
+          <b>Title</b>:${todo.attributes.title}
+          <b>Description:</b> ${todo.attributes.description}
+          <button onclick="completeTodo(${todo.id})">Complete</button>
+          <button onclick="deleteTodo(${todo.id})">Delete</button>
+          <button onclick="toggleEdit(this)">Edit</button>
+          <div class="editForm hidden">
+            <input type="text" placeholder="Title" value="${todo.attributes.title}"></input>
+            <input type="text" placeholder="Description" value="${todo.attributes.description}"></input>
+            <button onclick="editTodo(this, ${todo.id})">Confirm</button>
+          </div>
+        </li>`;
+      });
+      completedTodos.forEach((todo) => {
+        completedList.innerHTML += `<li>
+          <b>Title</b>:${todo.attributes.title}
+          <b>Description:</b> ${todo.attributes.description}
+          <button onclick="deleteTodo(${todo.id})">Delete</button>
+        </li>`;
       });
     }
   }
+};
+
+let completeTodo = async (id) => {
+  await axios.put(
+    `http://localhost:1337/api/todos/${id}`,
+    {
+      data: {
+        completed: true,
+      },
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+      },
+    }
+  );
+  renderPage();
+};
+
+let toggleEdit = (e) => {
+  let editForm = e.nextElementSibling;
+  editForm.classList.remove("hidden");
+};
+let editTodo = async (element, id) => {
+  let desc = element.previousElementSibling;
+  let title = desc.previousElementSibling;
+  console.log(title, desc);
+  await axios.put(
+    `http://localhost:1337/api/todos/${id}`,
+    {
+      data: {
+        title: title.value,
+        description: desc.value,
+      },
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+      },
+    }
+  );
+  renderPage();
+};
+
+let deleteTodo = async (id) => {
+  await axios.delete(`http://localhost:1337/api/todos/${id}`, {
+    headers: {
+      Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+    },
+  });
+  renderPage();
 };
 
 let register = async () => {
@@ -55,6 +133,7 @@ let addTodo = async () => {
       data: {
         title: todoTitle.value,
         description: todoDesc.value,
+        completed: false,
       },
     },
     {
